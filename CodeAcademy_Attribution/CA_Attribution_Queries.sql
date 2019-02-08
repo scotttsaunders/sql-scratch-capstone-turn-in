3b2c2e548f415be56179d2e022ded7ec3b0dfc48
scotttsaunders <46600763+scotttsaunders@users.noreply.github.com>
2019-02-08 15:22:05 -0500

    Codecademy generated from the project submission page.

---
+++ CodeAcademy_Attribution/CA_Attribution_Queries.sql
@@ -0,0 +1,125 @@
+/*SCOTT SAUNDERS ATTRIBUTION QUERIES*/
+
+/*question 1 */
+
+select
+count(distinct utm_campaign) as Distinct_Campaign
+from page_visits;
+
+select
+count(distinct utm_source) as Distinct_Source
+from page_visits;
+
+
+select distinct 
+utm_campaign,
+utm_source
+from page_visits;
+
+/*question 2 */
+
+select distinct 
+page_name
+from page_visits;
+
+/*question 3 */
+
+WITH first_touch AS (
+    SELECT user_id,
+        MIN(timestamp) as first_touch_at
+    FROM page_visits
+    GROUP BY user_id),
+ft_attr AS (
+  SELECT ft.user_id,
+         ft.first_touch_at,
+         pv.utm_source,
+         pv.utm_campaign
+  FROM first_touch ft
+  JOIN page_visits pv
+    ON ft.user_id = pv.user_id
+    AND ft.first_touch_at = pv.timestamp
+)
+SELECT ft_attr.utm_source,
+       ft_attr.utm_campaign,
+       COUNT(*)
+FROM ft_attr
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+/*question 4 */
+
+WITH last_touch AS (
+    SELECT user_id,
+        MAX(timestamp) as last_touch_at
+    FROM page_visits
+    GROUP BY user_id),
+lt_attr AS (
+  SELECT lt.user_id,
+         lt.last_touch_at,
+         pv.utm_source,
+         pv.utm_campaign,
+          pv.page_name
+  FROM last_touch lt
+  JOIN page_visits pv
+    ON lt.user_id = pv.user_id
+    AND lt.last_touch_at = pv.timestamp
+)
+SELECT lt_attr.utm_source,
+       lt_attr.utm_campaign,
+       COUNT(*)
+FROM lt_attr
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+/*question 5 */
+
+select 
+page_name,
+count(user_id)
+from page_visits
+group by
+page_name;
+
+/*question 6 */
+
+WITH last_touch AS (
+    SELECT user_id,
+        MAX(timestamp) as last_touch_at
+    FROM page_visits
+    where page_name = '4 - purchase'
+    GROUP BY user_id),
+lt_attr AS (
+  SELECT lt.user_id,
+         lt.last_touch_at,
+         pv.utm_source,
+         pv.utm_campaign,
+          pv.page_name
+  FROM last_touch lt
+  JOIN page_visits pv
+    ON lt.user_id = pv.user_id
+    AND lt.last_touch_at = pv.timestamp
+)
+SELECT lt_attr.utm_source,
+       lt_attr.utm_campaign,
+       COUNT(*)
+FROM lt_attr
+GROUP BY 1, 2
+ORDER BY 3 DESC;
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
